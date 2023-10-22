e4f50915980d83fb78beb8ef1b7d958090768c06
midnight1337 <kamil.koltows@gmail.com>
2023-10-22 15:08:28 +0200

    quick sort

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,14 +1,19 @@
 /*
-Quick sort - recursive algorithm based on 'divide and conquer' assumption, bigger problems are divided into smallers.
-As recursive algorithm it has to contain base condition that breaks further recursive calls, in quick sort there is if statement which checks if there is single element in array.
+Quick sort - It works on the principle of Divide and Conquer based on the idea of breaking down a list into several sub-lists,
+until each sublist consists of a single element in a manner that results into a sorted list.
+
 Algorithm uses a pivot, which helps to divide array into two parts called partitions, one for smaller numbers than pivot and one for larger numbers than pivot.
 Algorithm uses a limit, which helps to sort partitions into smaller and larger numbers than pivot, number at left side of limit are considered as smaller than pivot.
-If element at the right side of limit, is larger than pivot, then do nothing and increment iterator that points at array numbers in a for loop,
+
+Partitioning:
+1.If element at the right side of limit, is larger than pivot, then do nothing, increment iterator that points at array number,
 If it's smaller, then increment limit and swap number under limit with number under pointer iterator.
-At the end swap pivot with first element on the right from limit - this is place where pivot is placed to divide array into smaller and larger partitions.
-Array is now kinda sorted with one element on it's place.
-After partitioning, each of partition divided by pivot is passed as argument to quicksort function again so recursion happens. 
-Sorting is done when passed array contains only one element - we can check it by comparing indexes, if start >= stop.
+
+2.After loop swap element under pivot with first element on the right from limit
+(this is place where pivot is placed, with that array is 'divided' into smaller elements and larger elements than pivot)
+
+3.After partitioning, each of partition divided by pivot is passed as argument to quicksort function again so recursion happens. 
+Sorting is done when passed array contains only one element.
 
 limit  i              pivot  3 < 5  limit   i          pivot    1 < 5    limit   i       pivot   7 > 5   limit     i    pivot   ......
     |  3, 1, 7, 2, 0,  5              3  |  1, 7, 2, 0,  5                3, 1 | 7, 2, 0,  5             3, 1 | 7, 2, 0,  5
@@ -18,29 +23,34 @@ Time complexity (average case): O(N*log(N))
 #include <iostream>
 
 
-class MergeSort
+class QuickSort
 {
     public:
-        MergeSort() = default;
-        void merge_sort(int* array, int start, int stop);
+        QuickSort() = default;
+        void quick_sort(int* array, int start, int stop);
         int partition(int* array, int start, int stop);
 };
 
-void MergeSort::merge_sort(int* array, int start, int stop)
+void QuickSort::quick_sort(int* array, int start, int stop)
 {
+    //Base condition
     if (start >= stop) {return;}
 
+    //Get pivot index
     int pivot = partition(array, start, stop);
 
-    merge_sort(array, start, pivot - 1);
-    merge_sort(array, pivot + 1, stop);
+    //Call the same method till condition met
+    quick_sort(array, start, pivot - 1);
+    quick_sort(array, pivot + 1, stop);
 }
 
-int MergeSort::partition(int* array, int start, int stop)
+int QuickSort::partition(int* array, int start, int stop)
 {
+    //Create indexes of temporary pivot and limit
     int pivot = stop;
     int limit = start - 1;
 
+    //Loop over array and determine where limit should be placed
     for (int i = start; i < stop; i++)
     {
         if (array[i] < array[stop])
@@ -50,6 +60,7 @@ int MergeSort::partition(int* array, int start, int stop)
         }
     }
 
+    //Swap pivot with limit and return current pivot index
     limit++;
     std::swap(array[limit], array[stop]);
     
@@ -73,9 +84,9 @@ int main()
     int start = 0;
     int stop = array_size - 1;
 
-    MergeSort sort = MergeSort();
+    QuickSort sort = QuickSort();
 
-    sort.merge_sort(array, start, stop);
+    sort.quick_sort(array, start, stop);
 
     print(array, array_size);
 
